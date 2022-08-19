4ef6e01dd11eba39c46a45541d9165ea4119aefa
Orca <orca.zhang@yahoo.com>
2022-08-19 18:32:49 +0800

    :bookmark: minor commit

--- core/handler.go
+++ core/handler.go
@@ -1,29 +1,29 @@
 package core
 
+import "context"
+
 type Hanlder interface {
 	// 传入underlying，返回当前的，构成链式调用
 	New(h Hanlder) Hanlder
 
-	// 只有文件长度、HdrCRC32是预校验，如果成功返回true，失败返回false
-	TryRef(d []*DataInfo) ([]bool, error)
+	// 只有文件长度、HdrCRC32是预校验，如果成功返回新DataID，失败返回0
 	// 有文件长度、CRC32、MD5，成功返回引用的DataID，失败返回0，客户端发现DataID有变化，说明不需要上传数据
 	// 如果非预校验DataID传0，说明跳过了预校验
-	Ref(d []*DataInfo) ([]uint64, error)
+	Ref(c context.Context, d []*DataInfo) ([]uint64, error)
 	// 打包上传或者小文件，sn传-1，大文件sn从0开始，DataID不传默认创建一个新的
-	PutData(dataID uint64, sn int, buf []byte) (uint64, error)
+	PutData(c context.Context, dataID uint64, sn int, buf []byte) (uint64, error)
 	// 上传完数据以后，再创建元数据
-	PutDataInfo(d []*DataInfo) error
+	PutDataInfo(c context.Context, d []*DataInfo) error
+	// 只传一个参数说明是sn，传两个参数说明是offset+size
+	GetData(c context.Context, o *ObjectInfo, sn ...uint64) ([]uint64, error)
 
 	// 垃圾回收时有数据没有元数据引用的为脏数据（需要留出窗口时间），有元数据没有数据的为损坏数据
-	Create(o []*ObjectInfo) ([]uint64, error)
+	Create(c context.Context, o []*ObjectInfo) ([]uint64, error)
+	List(c context.Context, o *ObjectInfo, opt ListOptions) ([]*ObjectInfo, error)
 
-	// 只传一个参数说明是sn，传两个参数说明是offset+size
-	Get(*ObjectInfo, ...uint64) ([]uint64, error)
-	List(o *ObjectInfo, opt ListOptions) ([]*ObjectInfo, error)
+	Rename(c context.Context, o *ObjectInfo, newName string) error
+	MoveTo(c context.Context, o, newParent *ObjectInfo) error
 
-	Rename(o *ObjectInfo, newName string) error
-	MoveTo(o, newParent *ObjectInfo) error
-
-	Recycle(o *ObjectInfo) error
-	Delete(o *ObjectInfo) error
+	Recycle(c context.Context, o *ObjectInfo) error
+	Delete(c context.Context, o *ObjectInfo) error
 }
--- core/rw_handler.go
+++ core/rw_handler.go
@@ -1,6 +1,17 @@
 package core
 
+import "context"
+
 type RWHanlder struct {
+	mo MetaOperator
+	do DataOperator
+}
+
+func New() Hanlder {
+	return &RWHanlder{
+		mo: &DefaultMetaOperator{},
+		do: &DefaultDataOperator{},
+	}
 }
 
 // 传入underlying，返回当前的，构成链式调用
@@ -9,53 +20,49 @@ func (ch *RWHanlder) New(Hanlder) Hanlder {
 	return ch
 }
 
-// 只有文件长度、HdrCRC32是预校验，如果成功返回true，失败返回false
-func (ch *RWHanlder) TryRef(d []*DataInfo) ([]bool, error) {
-	return nil, nil
-}
-
+// 只有文件长度、HdrCRC32是预校验，如果成功返回新DataID，失败返回0
 // 有文件长度、CRC32、MD5，成功返回引用的DataID，失败返回0，客户端发现DataID有变化，说明不需要上传数据
 // 如果非预校验DataID传0，说明跳过了预校验
-func (ch *RWHanlder) Ref(d []*DataInfo) ([]uint64, error) {
+func (ch *RWHanlder) Ref(c context.Context, d []*DataInfo) ([]uint64, error) {
 	return nil, nil
 }
 
 // 打包上传或者小文件，sn传-1，大文件sn从0开始，DataID不传默认创建一个新的
-func (ch *RWHanlder) PutData(dataID uint64, sn int, buf []byte) (uint64, error) {
+func (ch *RWHanlder) PutData(c context.Context, dataID uint64, sn int, buf []byte) (uint64, error) {
 	return 0, nil
 }
 
 // 上传完数据以后，再创建元数据
-func (ch *RWHanlder) PutDataInfo(d []*DataInfo) error {
+func (ch *RWHanlder) PutDataInfo(c context.Context, d []*DataInfo) error {
 	return nil
-}
-
-// 垃圾回收时有数据没有元数据引用的为脏数据（需要留出窗口时间），有元数据没有数据的为损坏数据
-func (ch *RWHanlder) Create(o []*ObjectInfo) ([]uint64, error) {
-	return nil, nil
 }
 
 // 只传一个参数说明是sn，传两个参数说明是offset+size
-func (ch *RWHanlder) Get(o *ObjectInfo, sn ...uint64) ([]uint64, error) {
+func (ch *RWHanlder) GetData(c context.Context, o *ObjectInfo, sn ...uint64) ([]uint64, error) {
 	return nil, nil
 }
 
-func (ch *RWHanlder) List(o *ObjectInfo, opt ListOptions) ([]*ObjectInfo, error) {
+// 垃圾回收时有数据没有元数据引用的为脏数据（需要留出窗口时间），有元数据没有数据的为损坏数据
+func (ch *RWHanlder) Create(c context.Context, o []*ObjectInfo) ([]uint64, error) {
 	return nil, nil
 }
 
-func (ch *RWHanlder) Rename(o *ObjectInfo, newName string) error {
+func (ch *RWHanlder) List(c context.Context, o *ObjectInfo, opt ListOptions) ([]*ObjectInfo, error) {
+	return nil, nil
+}
+
+func (ch *RWHanlder) Rename(c context.Context, o *ObjectInfo, newName string) error {
 	return nil
 }
 
-func (ch *RWHanlder) MoveTo(o, newParent *ObjectInfo) error {
+func (ch *RWHanlder) MoveTo(c context.Context, o, newParent *ObjectInfo) error {
 	return nil
 }
 
-func (ch *RWHanlder) Recycle(o *ObjectInfo) error {
+func (ch *RWHanlder) Recycle(c context.Context, o *ObjectInfo) error {
 	return nil
 }
 
-func (ch *RWHanlder) Delete(o *ObjectInfo) error {
+func (ch *RWHanlder) Delete(c context.Context, o *ObjectInfo) error {
 	return nil
 }
