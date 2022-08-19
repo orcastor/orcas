37daccc8b3b21bafeaba1977c39c5e7997a21e22
Orca <orca.zhang@yahoo.com>
2022-08-19 15:14:38 +0800

    :car: minor changes

--- core/data.go
+++ core/data.go
@@ -13,27 +13,40 @@ import (
 	"github.com/orca-zhang/ecache"
 )
 
-const interval = 5 * time.Second
+type Option struct {
+	Sync bool
+}
+
+type DataOperator interface {
+	SetOption(opt Option)
+	Write(c context.Context, fileName string, buf []byte) error
+	Read(c context.Context, fileName string) ([]byte, error)
+	ReadBytes(c context.Context, fileName string, offset, size int64) ([]byte, error)
+}
+
+const interval = time.Second
 
 var buffer = ecache.NewLRUCache(16, 1024, interval)
 
 func init() {
 	buffer.Inspect(func(action int, key string, iface *interface{}, bytes []byte, status int) {
+		// evicted / updated / deleted
 		if (action == ecache.PUT && status <= 0) || (action == ecache.DEL && status == 1) {
-			w := (*iface).(*AsyncHandle)
+			w := (*iface).(*AsyncHandles)
 			w.B.Flush()
 			w.F.Close()
 		}
 	})
 
 	go func() {
+		// manual evict expired items
 		for {
 			keys := []string{}
 			buffer.Walk(func(key string, iface *interface{}, bytes []byte, expireAt int64) bool {
 				if expireAt < time.Now().UnixNano() {
 					keys = append(keys, key)
 				}
-				return true // 是否继续遍历
+				return true
 			})
 			for _, key := range keys {
 				buffer.Del(key)
@@ -43,20 +56,17 @@ func init() {
 	}()
 }
 
-type AsyncHandle struct {
+func HasInflight() (b bool) {
+	buffer.Walk(func(key string, iface *interface{}, bytes []byte, expireAt int64) bool {
+		b = true
+		return false
+	})
+	return b
+}
+
+type AsyncHandles struct {
 	F *os.File
 	B *bufio.Writer
-}
-
-type Option struct {
-	Async bool
-}
-
-type DataOperator interface {
-	SetOption(opt Option)
-	Write(c context.Context, fileName string, buf []byte) error
-	Read(c context.Context, fileName string) ([]byte, error)
-	ReadBytes(c context.Context, fileName string, offset, size int64) ([]byte, error)
 }
 
 type DefaultDataOperator struct {
@@ -84,7 +94,7 @@ func (ddo *DefaultDataOperator) Write(c context.Context, fileName string, buf []
 	if err != nil {
 		return err
 	}
-	if !ddo.Options.Async {
+	if ddo.Options.Sync {
 		defer f.Close()
 	}
 
@@ -94,10 +104,11 @@ func (ddo *DefaultDataOperator) Write(c context.Context, fileName string, buf []
 		return err
 	}
 
-	if ddo.Options.Async {
-		buffer.Put(path, &AsyncHandle{F: f, B: b})
-	} else {
+	if ddo.Options.Sync {
 		err = b.Flush()
+	} else {
+		go b.Flush()
+		buffer.Put(path, &AsyncHandles{F: f, B: b})
 	}
 	return err
 }
@@ -120,13 +131,16 @@ func (ddo *DefaultDataOperator) ReadBytes(c context.Context, fileName string, of
 	}
 	defer f.Close()
 
-	f.Seek(offset, io.SeekEnd)
+	f.Seek(offset, io.SeekStart)
 
 	var buf []byte
 	if size == -1 {
 		fi, err := f.Stat()
 		if err != nil {
 			return nil, err
+		}
+		if fi.Size() < offset {
+			return nil, nil
 		}
 
 		buf = make([]byte, fi.Size()-offset)
