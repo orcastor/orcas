160db473d3191ae46323920eb91df8adf1912a2c
Orca <orca.zhang@yahoo.com>
2022-08-22 19:43:43 +0800

    :whale2: update

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -1,9 +1,10 @@
 package sdk
 
 type Config struct {
-	DataSync     int    // Power-off Protection Method，强制每次写入数据后刷到磁盘
-	TryRef       int    // None / Ref / TryRef+Ref
-	Conflict     int    // Throw / Merge / Rename，同名冲突后
-	ConflictTail string // "-副本" / "{\d}"
-	SSideDecompr int    // 服务端解压，PS：必须是非加密数据
+	DataSync int    // Power-off Protection Method，强制每次写入数据后刷到磁盘
+	RefLevel int    // None / Ref / TryRef+Ref
+	Conflict int    // Throw / Merge / Rename，同名冲突后
+	NameTail string // 重命名尾巴，"-副本" / "{\d}"
+	BEDecmpr int    // 后端解压，PS：必须是非加密数据
+	WiseCmpr int    // 智能压缩，根据文件类型决定是否压缩
 }
