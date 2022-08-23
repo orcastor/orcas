c0f2bd785290bdf2eade416c14b1043f1cf2e145
Orca <orca.zhang@yahoo.com>
2022-08-23 12:06:02 +0800

    :golf: rename

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -1,10 +1,11 @@
 package sdk
 
 type Config struct {
-	DataSync int    // Power-off Protection Method，强制每次写入数据后刷到磁盘
+	DataSync bool   // Power-off Protection Method，强制每次写入数据后刷到磁盘
 	RefLevel int    // None / Ref / TryRef+Ref
+	RefLimit int    // 秒传大小限制
 	Conflict int    // Throw / Merge / Rename，同名冲突后
 	NameTail string // 重命名尾巴，"-副本" / "{\d}"
-	BEDecmpr int    // 后端解压，PS：必须是非加密数据
-	WiseCmpr int    // 智能压缩，根据文件类型决定是否压缩
+	BEDecmpr bool   // 后端解压，PS：必须是非加密数据
+	WiseCmpr bool   // 智能压缩，根据文件类型决定是否压缩
 }
