6e729dfc1e57b2f679c4f7d385b5f927e5dd73b3
Orca <orca.zhang@yahoo.com>
2022-08-25 16:19:18 +0800

    :sparkle: rename

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -3,7 +3,7 @@ package sdk
 type Config struct {
 	DataSync bool   // Power-off Protection Method，强制每次写入数据后刷到磁盘
 	RefLevel int    // None / Ref / TryRef+Ref
-	RefLimit int    // 秒传大小限制
+	RefThres int    // 秒传大小限制
 	Conflict int    // Throw / Merge / Rename，同名冲突后
 	NameTail string // 重命名尾巴，"-副本" / "{\d}"
 	BEDecmpr bool   // 后端解压，PS：必须是非加密数据
