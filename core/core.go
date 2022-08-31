d63fa8d6f3d192aa6069e44b4d98fc4626079e0e
Orca <orca.zhang@yahoo.com>
2022-08-31 22:06:45 +0800

    :books: fix typo

--- core/core.go
+++ core/core.go
@@ -13,9 +13,9 @@ type ListOptions struct {
 	Brief int    // 显示更少内容(只在网络传输层，节省流量时有效)，0: FULL(default), 1: without EXT, 2:only ID
 }
 
-type Hanlder interface {
+type Handler interface {
 	// 传入underlying，返回当前的，构成链式调用
-	New(h Hanlder) Hanlder
+	New(h Handler) Handler
 	Close()
 
 	SetOptions(opt Options)
@@ -48,16 +48,16 @@ type Hanlder interface {
 	Delete(c Ctx, id int64) error
 }
 
-type RWHanlder struct {
+type RWHandler struct {
 	mo    MetaOperator
 	do    DataOperator
 	ig    *idgen.IDGen
 	bktID int64
 }
 
-func NewRWHandler(bktID int64) Hanlder {
+func NewRWHandler(bktID int64) Handler {
 	acm := &DefaultAccessCtrlMgr{}
-	return &RWHanlder{
+	return &RWHandler{
 		mo:    NewDefaultMetaOperator(acm),
 		do:    NewDefaultDataOperator(acm),
 		ig:    idgen.NewIDGen(nil, 0), // 需要改成配置
@@ -66,33 +66,33 @@ func NewRWHandler(bktID int64) Hanlder {
 }
 
 // 传入underlying，返回当前的，构成链式调用
-func (ch *RWHanlder) New(Hanlder) Hanlder {
+func (ch *RWHandler) New(Handler) Handler {
 	// 忽略下层handler
 	return ch
 }
 
-func (ch *RWHanlder) Close() {
+func (ch *RWHandler) Close() {
 	ch.do.Close()
 	ch.mo.Close()
 }
 
-func (ch *RWHanlder) SetOptions(opt Options) {
+func (ch *RWHandler) SetOptions(opt Options) {
 	ch.do.SetOptions(opt)
 }
 
-func (ch *RWHanlder) PutBkt(c Ctx, o []*BucketInfo) error {
+func (ch *RWHandler) PutBkt(c Ctx, o []*BucketInfo) error {
 	return ch.mo.PutBkt(c, o)
 }
 
 // 只有文件长度、HdrCRC32是预Ref，如果成功返回新DataID，失败返回0
 // 有文件长度、CRC32、MD5，成功返回引用的DataID，失败返回0，客户端发现DataID有变化，说明不需要上传数据
 // 如果非预Ref DataID传0，说明跳过了预Ref
-func (ch *RWHanlder) Ref(c Ctx, d []*DataInfo) ([]int64, error) {
+func (ch *RWHandler) Ref(c Ctx, d []*DataInfo) ([]int64, error) {
 	return ch.mo.RefData(c, ch.bktID, d)
 }
 
 // 打包上传或者小文件，sn传-1，大文件sn从0开始，DataID不传默认创建一个新的
-func (ch *RWHanlder) PutData(c Ctx, dataID int64, sn int, buf []byte) (int64, error) {
+func (ch *RWHandler) PutData(c Ctx, dataID int64, sn int, buf []byte) (int64, error) {
 	if dataID == 0 {
 		if len(buf) <= 0 {
 			dataID = EmptyDataID
@@ -104,7 +104,7 @@ func (ch *RWHanlder) PutData(c Ctx, dataID int64, sn int, buf []byte) (int64, er
 }
 
 // 上传完数据以后，再创建元数据
-func (ch *RWHanlder) PutDataInfo(c Ctx, d []*DataInfo) (ids []int64, err error) {
+func (ch *RWHandler) PutDataInfo(c Ctx, d []*DataInfo) (ids []int64, err error) {
 	for _, x := range d {
 		if x.ID == 0 {
 			x.ID, _ = ch.ig.New()
@@ -114,12 +114,12 @@ func (ch *RWHanlder) PutDataInfo(c Ctx, d []*DataInfo) (ids []int64, err error) 
 	return ids, ch.mo.PutData(c, ch.bktID, d)
 }
 
-func (ch *RWHanlder) GetDataInfo(c Ctx, id int64) (*DataInfo, error) {
+func (ch *RWHandler) GetDataInfo(c Ctx, id int64) (*DataInfo, error) {
 	return ch.mo.GetData(c, ch.bktID, id)
 }
 
 // 只传一个参数说明是sn，传两个参数说明是sn+offset，传三个参数说明是sn+offset+size
-func (ch *RWHanlder) GetData(c Ctx, id int64, sn int, offset ...int) ([]byte, error) {
+func (ch *RWHandler) GetData(c Ctx, id int64, sn int, offset ...int) ([]byte, error) {
 	switch len(offset) {
 	case 0:
 		return ch.do.Read(c, ch.bktID, id, sn)
@@ -130,13 +130,13 @@ func (ch *RWHanlder) GetData(c Ctx, id int64, sn int, offset ...int) ([]byte, er
 }
 
 // 用于非文件内容的扫描，只看文件是否存在，大小是否合适
-func (ch *RWHanlder) FileSize(c Ctx, dataID int64, sn int) (int64, error) {
+func (ch *RWHandler) FileSize(c Ctx, dataID int64, sn int) (int64, error) {
 	return ch.do.FileSize(c, ch.bktID, dataID, sn)
 }
 
 // 垃圾回收时有数据没有元数据引用的为脏数据（需要留出窗口时间），有元数据没有数据的为损坏数据
 // PID支持用补码来直接引用当次还未上传的对象的ID
-func (ch *RWHanlder) Put(c Ctx, o []*ObjectInfo) ([]int64, error) {
+func (ch *RWHandler) Put(c Ctx, o []*ObjectInfo) ([]int64, error) {
 	for _, x := range o {
 		if x.ID == 0 {
 			x.ID, _ = ch.ig.New()
@@ -150,27 +150,27 @@ func (ch *RWHanlder) Put(c Ctx, o []*ObjectInfo) ([]int64, error) {
 	return ch.mo.PutObj(c, ch.bktID, o)
 }
 
-func (ch *RWHanlder) Get(c Ctx, ids []int64) ([]*ObjectInfo, error) {
+func (ch *RWHandler) Get(c Ctx, ids []int64) ([]*ObjectInfo, error) {
 	return ch.mo.GetObj(c, ch.bktID, ids)
 }
 
-func (ch *RWHanlder) List(c Ctx, pid int64, opt ListOptions) ([]*ObjectInfo, int64, string, error) {
+func (ch *RWHandler) List(c Ctx, pid int64, opt ListOptions) ([]*ObjectInfo, int64, string, error) {
 	return ch.mo.ListObj(c, ch.bktID, pid, opt.Word, opt.Delim, opt.Order, opt.Count, 0)
 }
 
 // 如果存在同名文件，会报错：Error: stepping, UNIQUE constraint failed: obj.name (19)
-func (ch *RWHanlder) Rename(c Ctx, id int64, name string) error {
+func (ch *RWHandler) Rename(c Ctx, id int64, name string) error {
 	return ch.mo.SetObj(c, ch.bktID, []string{"name"}, &ObjectInfo{ID: id, Name: name})
 }
 
-func (ch *RWHanlder) MoveTo(c Ctx, id, pid int64) error {
+func (ch *RWHandler) MoveTo(c Ctx, id, pid int64) error {
 	return ch.mo.SetObj(c, ch.bktID, []string{"pid"}, &ObjectInfo{ID: id, PID: pid})
 }
 
-func (ch *RWHanlder) Recycle(c Ctx, id int64) error {
+func (ch *RWHandler) Recycle(c Ctx, id int64) error {
 	return ch.mo.SetObj(c, ch.bktID, []string{"status"}, &ObjectInfo{ID: id, Status: OBJ_RECYCLED})
 }
 
-func (ch *RWHanlder) Delete(c Ctx, id int64) error {
+func (ch *RWHandler) Delete(c Ctx, id int64) error {
 	return ch.mo.SetObj(c, ch.bktID, []string{"status"}, &ObjectInfo{ID: id, Status: OBJ_DELETED})
 }
