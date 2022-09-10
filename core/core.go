282b6d8400d0ff72b659f90399ba5ae960934dac
Orca <orca.zhang@yahoo.com>
2022-09-11 00:39:51 +0800

    :pear: update comment

--- core/core.go
+++ core/core.go
@@ -9,7 +9,7 @@ import (
 type ListOptions struct {
 	Word  string // 过滤词，支持通配符*和?
 	Delim string // 分隔符，每次请求后返回，原样回传即可
-	Type  int    // 对象类型，0: 不过滤(default), 1: dir, 2: file, 3: version, 4: preview(thumb/m3u8/pdf)
+	Type  int    // 对象类型，-1: malformed, 0: 不过滤(default), 1: dir, 2: file, 3: version, 4: preview(thumb/m3u8/pdf)
 	Count int    // 查询个数
 	Order string // 排序方式，id/mtime/name/size/type 前缀 +: 升序（默认） -: 降序
 	Brief int    // 显示更少内容(只在网络传输层，节省流量时有效)，0: FULL(default), 1: without EXT, 2:only ID
