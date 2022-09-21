fe45ad7360802892b102c29350a091cd2a2a3bec
Orca <orca.zhang@yahoo.com>
2022-09-21 19:39:50 +0800

    :fallen_leaf: refactor: move admin out

--- core/auth.go
+++ core/auth.go
@@ -24,31 +24,14 @@ type AccessCtrlMgr interface {
 	CheckRole(c Ctx, role int) error
 }
 
-type Admin interface {
-	ChPwd(c Ctx, uid int64, pwd, newPwd string) error
-	ChRole(c Ctx, uid int64, role int) error
-
-	AddUsr(c Ctx, usr, pwd string) error
-	DelUsr(c Ctx, usr, pwd string) error
-
-	NewOTP(c Ctx) (string, string)
-}
-
 type DefaultAccessCtrlMgr struct {
 }
 
-func (dac *DefaultAccessCtrlMgr) CheckPermission(c Ctx, action int, bktID int64) error {
+func (dacm *DefaultAccessCtrlMgr) CheckPermission(c Ctx, action int, bktID int64) error {
+	// check owner of bucket
 	return nil
 }
 
-func (dac *DefaultAccessCtrlMgr) CheckRole(c Ctx, role int) error {
+func (dacm *DefaultAccessCtrlMgr) CheckRole(c Ctx, role int) error {
 	return nil
-}
-
-func Auth(c Ctx, usr, pwd, otp string) error {
-	// pwd -> rsa decode
-	// pwd from db or cache
-	// pbkdf2 check
-	// set uid & key to ctx
-	return ERR_AUTH_FAILED
 }
