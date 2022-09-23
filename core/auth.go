e9b50d5fd5adf098e9b6f0ba8327ad30566dba5d
Orca <orca.zhang@yahoo.com>
2022-09-23 13:22:16 +0800

    :spades: feat: add `Login`

--- core/auth.go
+++ core/auth.go
@@ -1,22 +1,24 @@
 package core
 
-const (
-	NA = 1 << iota
-	DR
-	DW
-	DD
-	MDR
-	MDW
-	MDD
+import "context"
 
-	DRW  = DR | DW
-	MDRW = MDR | MDW
-	ALL  = DRW | DD | MDRW | MDD
+const (
+	NA  = 1 << iota
+	DR  // 数据读取
+	DW  // 数据写入
+	DD  // 数据删除
+	MDR // 元数据读取
+	MDW // 元数据写入
+	MDD // 元数据删除
+
+	DRW  = DR | DW               // 数据读写
+	MDRW = MDR | MDW             // 元数据读写
+	ALL  = DRW | DD | MDRW | MDD // 数据、元数据读写删
 )
 
 const (
-	USER = iota
-	ADMIN
+	USER  = iota // 普通用户
+	ADMIN        // 管理员
 )
 
 type AccessCtrlMgr interface {
@@ -34,4 +36,33 @@ func (dacm *DefaultAccessCtrlMgr) CheckPermission(c Ctx, action int, bktID int64
 
 func (dacm *DefaultAccessCtrlMgr) CheckRole(c Ctx, role int) error {
 	return nil
+}
+
+func userInfo2Ctx(c Ctx, u *UserInfo) Ctx {
+	return context.WithValue(c, "o", map[string]interface{}{
+		"uid": u.ID,
+		"key": u.Key,
+	})
+}
+
+func getUID(c Ctx) int64 {
+	if v, ok := c.Value("o").(map[string]interface{}); ok {
+		if uid, okk := v["id"]; okk {
+			if u, okkk := uid.(int64); okkk {
+				return u
+			}
+		}
+	}
+	return 0
+}
+
+func getKey(c Ctx) string {
+	if v, ok := c.Value("o").(map[string]interface{}); ok {
+		if key, okk := v["key"]; okk {
+			if k, okkk := key.(string); okkk {
+				return k
+			}
+		}
+	}
+	return ""
 }
