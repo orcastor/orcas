2347fb604d750ae2af51b281fca811cd97f5e082
Orca <orca.zhang@yahoo.com>
2022-09-28 17:38:42 +0800

    :pear: feat(cors)

--- rpc/middleware/cors.go
+++ rpc/middleware/cors.go
@@ -8,9 +8,10 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+		c.Header("Access-Control-Allow-Headers", "Content-Type, AccessToken, X-CSRF-Token, Authorization, Token")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Max-Age", "3600")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
